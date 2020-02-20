b01868a91c3cb77f13bc2717d3628f175b040a6d
Joel Hobgood <jhobgoo@gmail.com>
2020-02-20 06:09:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(other_deli)
+  if other_deli.length == 0
+    puts "The line is currently empty."
+  elsif other_deli.length > 0
+    string = "The line is currently:"
+    other_deli.each_with_index do |element, index|
+      string += " #{index + 1}. #{element}"
+      end
+    puts string
+  end
+end
+
+def take_a_number(other_deli, element)
+  length = other_deli.length
+  other_deli.push(element)
+  puts "Welcome, #{element}. You are number #{length} in line."
+end
